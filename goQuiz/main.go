8f05f801e2462e93264881f0ba3bbba2b6fcad25
iwita <atzenetopoulos@gmail.com>
2020-03-05 16:03:47 +0200

    added the timer in the goQuiz

--- goQuiz/main.go
+++ goQuiz/main.go
@@ -1,11 +1,14 @@
 package main
 
+//TODO Refactor the code in order to be easier to be tested with Unit tests
+
 import (
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -14,6 +17,9 @@ func main() {
 	//Usage flag.String(flagname, default,help output)
 	csvFileName := flag.String("csv", "problems.csv",
 		"a csv file in the format of 'question,answer'")
+
+	// Time limit flag
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 
 	//parse the flag provided
 	flag.Parse()
@@ -25,7 +31,7 @@ func main() {
 	}
 	// I/O reader
 	// It is the most common interface
-	//It cal also be used for reading HTTP respnses or other byte slices
+	// It cal also be used for reading HTTP respnses or other byte slices
 
 	r := csv.NewReader(file)
 
@@ -38,24 +44,46 @@ func main() {
 
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	// <-timer.C blocks the code here until it gets a mesage from the channel
+
 	correct := 0
 
+	//Use a label to characterize the loops
+problemloop:
 	//Range: provides both index and value (i.e. i, p) for each entry
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.question)
-		var answer string
-
-		//It is not appropriate for other cases (i.e string answers)
-		//Trims the spaces
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.answer {
-			fmt.Println("Correct")
-			correct++
+		fmt.Printf("Problem #%d: %s = ", i+1, p.question)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			//It is not appropriate for other cases (i.e string answers)
+			//Trims the spaces
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer // when an answer is commited it is send in the channel
+		}()
+		select {
+		// first come first served
+		// if the timer ends before an answer is commited, the timer case is seleced
+		// else if an answer is submited firstly, the answer case is selected
+		case <-timer.C:
+			//fmt.Printf("\nYou scored %d out of %d\n", correct, len(problems))
+			//return
+			fmt.Println()
+			break problemloop
+		case answer := <-answerCh:
+			if answer == p.answer {
+				//fmt.Println("Correct")
+				correct++
+			}
 		}
 
 	}
 
-	fmt.Printf("You scored %d out of %d", correct, len(problems))
+	// a new line is not needed here in contrast with the line 65
+	// because this is the case all answers are sumbitted,
+	// and the user must have hit enter in his last answer
+	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
