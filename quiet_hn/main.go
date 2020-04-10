555f0927221a723acb85fff51e6718ed7ab29de7
Achilleas Tzenetopoulos <achilleas.tze@gmail.com>
2020-04-10 13:37:52 +0300

    COncurrency without getting an exact number of stories

---
+++ quiet_hn/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/iwita/go-exercises/quiet_hn/hn"
+)
+
+func main() {
+	// parse flags
+	var port, numStories int
+	flag.IntVar(&port, "port", 3000, "the port to start the web server on")
+	flag.IntVar(&numStories, "num_stories", 30, "the number of top stories to display")
+	flag.Parse()
+
+	tpl := template.Must(template.ParseFiles("./index.gohtml"))
+
+	http.HandleFunc("/", handler(numStories, tpl))
+
+	// Start the server
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+func handler(numStories int, tpl *template.Template) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		stories, err := getTopStories(numStories)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		data := templateData{
+			Stories: stories,
+			Time:    time.Now().Sub(start),
+		}
+		err = tpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, "Failed to process the template", http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+type result struct {
+	idx  int
+	item item
+	err  error
+}
+
+var resultCh = make(chan result)
+
+func getTopStories(numStories int) ([]item, error) {
+	var client hn.Client
+	ids, err := client.TopItems()
+	if err != nil {
+		return nil, errors.New("Failed to load top stories")
+	}
+	//var stories []item
+	for idx := 0; idx < numStories; idx++ {
+		go func(idx int) {
+			hnItem, err := client.GetItem(ids[idx])
+			if err != nil {
+				resultCh <- result{idx: idx, err: err}
+			}
+			resultCh <- result{idx: idx, item: parseHNItem(hnItem)}
+		}(idx)
+	}
+	var results []result
+	for i := 0; i < numStories; i++ {
+		results = append(results, <-resultCh)
+	}
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].idx < results[j].idx
+	})
+	// sort the not links
+	var stories []item
+	for _, res := range results {
+		if res.err != nil {
+			continue
+		}
+		if isStoryLink(res.item) {
+			stories = append(stories, res.item)
+		}
+	}
+	return stories, nil
+}
+
+// if isStoryLink(item) {
+// 	stories = append(stories, item)
+// 	if len(stories) >= numStories {
+// 		break
+// 	}
+// }
+// }
+// return stories, nil
+
+func isStoryLink(item item) bool {
+	return item.Type == "story" && item.URL != ""
+}
+
+func parseHNItem(hnItem hn.Item) item {
+	ret := item{Item: hnItem}
+	url, err := url.Parse(ret.URL)
+	if err == nil {
+		ret.Host = strings.TrimPrefix(url.Hostname(), "www.")
+	}
+	return ret
+}
+
+// item is the same as the hn.Item, but adds the Host field
+type item struct {
+	hn.Item
+	Host string
+}
+
+type templateData struct {
+	Stories []item
+	Time    time.Duration
+}
